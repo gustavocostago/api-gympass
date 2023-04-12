5a2e6d497adaa8f3bb3f40739373e98ee4ded394
Gustavo Costa <gustavocosta.cba@gmail.com>
2023-04-12 15:54:48 -0400

    feat: fastify-swagger-ui

--- src/app.ts
+++ src/app.ts
@@ -2,11 +2,25 @@ import fastify from 'fastify'
 import { appRoutes } from './http/routes'
 import { ZodError } from 'zod'
 import { env } from 'env'
+import fastifySwagger from '@fastify/swagger'
+import fastifySwaggerUi from '@fastify/swagger-ui'
 
 export const app = fastify({
   logger: false,
 })
 
+app.register(fastifySwagger, {
+  swagger: {
+    info: {
+      title: 'Api Gym Pass',
+      version: 'v1',
+      description: 'Documentação sobre as endpoints do back-end',
+    },
+  },
+})
+app.register(fastifySwaggerUi, {
+  prefix: '/v1/docs',
+})
 app.register(appRoutes)
 
 app.setErrorHandler((error, _request, reply) => {
--- src/http/routes.ts
+++ src/http/routes.ts
@@ -2,5 +2,56 @@ import { FastifyInstance } from 'fastify'
 import { register } from './controllers/register'
 
 export async function appRoutes(app: FastifyInstance) {
-  app.post('/users', register)
+  app.post(
+    '/users',
+    {
+      schema: {
+        description: 'Realiza a criação de usuário',
+        tags: ['Usuários'],
+        params: {
+          type: 'object',
+          properties: {
+            name: {
+              type: 'string',
+              description: 'Nome de usuário',
+            },
+            email: {
+              type: 'string',
+              description: 'E-mail válido',
+            },
+            password: {
+              type: 'string',
+              description: 'Senha com no mínimo 6 caracters',
+            },
+          },
+        },
+        body: {
+          type: 'object',
+          properties: {
+            hello: { type: 'string' },
+            obj: {
+              type: 'object',
+              properties: {
+                some: { type: 'string' },
+              },
+            },
+          },
+        },
+        response: {
+          201: {
+            description: 'Successful response',
+            type: 'object',
+          },
+          400: {
+            description: 'Bad Request',
+            type: 'object',
+            properties: {
+              message: { type: 'string' },
+            },
+          },
+        },
+      },
+    },
+    register
+  )
 }
