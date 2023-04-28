0312e8b710a2d9ff9bfffc2c8526ea92257c7b05
Gustavo Costa <gustavocosta.cba@gmail.com>
2023-04-28 10:35:46 -0400

    feat: getUserProfile

--- src/http/controllers/profile.ts
+++ src/http/controllers/profile.ts
@@ -2,11 +2,14 @@ import { makeGetUserProfileService } from '@/services/factories/make-get-user-pr
 import { FastifyReply, FastifyRequest } from 'fastify'
 
 export async function profile(request: FastifyRequest, reply: FastifyReply) {
-  await request.jwtVerify()
   const getUserProfile = makeGetUserProfileService()
   const { user } = await getUserProfile.execute({
     userId: request.user.sub,
   })
+  const data = {
+    ...user,
+    password_hash: undefined,
+  }
 
-  return reply.status(200).send(user)
+  return reply.status(200).send(data)
 }
