3f512a1ef75c3b49862f6a7b60ce353d9a2815b9
Gustavo Costa <gustavocosta.cba@gmail.com>
2023-06-05 11:16:59 -0400

    fix: routes

--- src/http/controllers/users/routes.ts
+++ src/http/controllers/users/routes.ts
@@ -1,14 +1,11 @@
-import { FastifyInstance, FastifyReply } from 'fastify'
+import { FastifyInstance } from 'fastify'
 import { register } from './register'
 import { authenticate } from './authenticate'
 import { profile } from './profile'
 import { verifyJWT } from '@/http/controllers/middlewares/verify-jwt'
 import { refresh } from './refresh'
 
-export async function usersRoutes(app: FastifyInstance, reply: FastifyReply) {
-  app.get('/', () => {
-    reply.status(200).send({ message: 'teste' })
-  })
+export async function usersRoutes(app: FastifyInstance) {
   app.post('/users', register)
   app.post('/sessions', authenticate)
 
