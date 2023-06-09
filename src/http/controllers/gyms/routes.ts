6060fad7cbaeb289d90c34dbbceaba38beee3a3c
Gustavo Costa <gustavocosta.cba@gmail.com>
2023-06-09 09:12:58 -0400

    test deploy automatico

--- src/http/controllers/gyms/routes.ts
+++ src/http/controllers/gyms/routes.ts
@@ -1,4 +1,4 @@
-import { FastifyInstance } from 'fastify'
+import { FastifyInstance, FastifyReply, FastifyRequest } from 'fastify'
 import { verifyJWT } from '../middlewares/verify-jwt'
 import { search } from './search'
 import { nearby } from './nearby'
@@ -6,6 +6,10 @@ import { create } from './create'
 import { verifyUserRole } from '../middlewares/verify-user-role'
 
 export async function gymRoutes(app: FastifyInstance) {
+  async function hello(req: FastifyRequest, res: FastifyReply) {
+    res.status(200).send('hello')
+  }
+  app.get('/', hello)
   app.addHook('onRequest', verifyJWT)
 
   app.get('/gyms/search', search)
